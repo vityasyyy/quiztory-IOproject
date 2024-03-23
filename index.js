1322b8e60e9826fe1c7bb592a9edb185d1e0b440
vityasyyy <argyavityasy1208@gmail.com>
2024-03-23 23:11:09 +0700

    Updated index.js accordingly

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
+require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const path = require('path')
+const flash = require('connect-flash')
 const ejsMate = require('ejs-mate')
+const MongoStore = require('connect-mongo')
+const session = require('express-session')
+const LocalStrategy = require('passport-local')
+const passport = require('passport')
 
 const ExpressError = require('./Middleware/ExpressError')
+
+const User = require('./Models/usersSchema')
+
+const sectionsRoutes = require('./Routes/sectionsRoutes')
+const questionsRoutes = require('./Routes/questionsRoutes')
+const usersRoutes = require('./Routes/usersRoutes')
+
+const app = express()
+
+const dbURL = process.env.DBURL
+const urlParser = {userNewURLParser: true}
 mongoose.set('strictQuery', true);
 mongoose.connect('mongodb://127.0.0.1:27017/quiztory')
     .then(() => {
@@ -14,15 +31,56 @@ mongoose.connect('mongodb://127.0.0.1:27017/quiztory')
         console.log(err); 
     })
 
-const app = express()
+const secret = process.env.SECRET
+
+const store = MongoStore.create({
+    mongoUrl: dbURL,
+    touchAfter: 24 * 60 * 60,
+    crypto: {
+        secret,
+    }
+})
+store.on('error', function(e) {
+    console.log('Session store error', e)
+})
+const sessionConfig = {
+    secret,
+    resave: false,
+    saveUninitialized: false,
+    cookies: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+
+app.use(session(sessionConfig));
+app.use(flash());
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+passport.use(new LocalStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
 
 app.engine('ejs', ejsMate)
 app.set('view engine' , 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
-app.get('/', (req, res) => {
-    res.send("HALLOOO")
+app.use(express.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname, 'Public')))
+
+app.use((req,res,next) => {
+    res.locals.signedInUser = req.user; 
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
 })
+
+app.use('/', usersRoutes)
+app.use('/sections', sectionsRoutes)
+app.use('/sections/:id/question', questionsRoutes)
 
 app.all('*', (req, res, next) => {
     next(new ExpressError("Page Not Found", 404))
